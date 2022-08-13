b7dd6dca3a80a13a910cca3767df137469d9b2d8
Mina Victor <107629210+MinaVictor1@users.noreply.github.com>
2022-08-13 02:05:38 +0200

    Add files via upload

---
+++ Search/Games.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Mavenproject2 {
+
+   public static void main(String[] args) {
+    //Scanner in =new Scanner(System.in);
+    //int x=in.nextInt();
+    System.out.println(100%25);
+  }  
+}
